fb54b5a0dc0b217c7b51cc16592803a24b729821
Louie Cervantes <cervantes.louie13@gmail.com>
2023-06-27 05:39:15 -0700

    fixed typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const counter = document.getElementById('count');
 const countUrl = 'https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.on.aws/'
-aupdateVisitCount();
+updateVisitCount();
 
 function updateVisitCount() {
     fetch(countUrl)

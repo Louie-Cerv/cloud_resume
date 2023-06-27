c73deed5f1173199673911e1f1461208682cd83e
Louie Cervantes <cervantes.louie13@gmail.com>
2023-06-27 05:49:27 -0700

    removed a line

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const counter = document.getElementById('count');
 const countUrl = 'https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.on.aws/'
-updateVisitCount();
 
 function updateVisitCount() {
     fetch(countUrl)

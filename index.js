c211e0bb61fbfb27a74cb424da842a31378d208c
Louie Cervantes <cervantes.louie13@gmail.com>
2023-06-27 06:07:55 -0700

    changed element ID name

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const counter = document.getElementById('count');
+const countElement = document.getElementById('count');
 const countUrl = 'https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.on.aws/'
+
+updateVisitCount();
 
 function updateVisitCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        counter.innerHTML = res.visits;
+        countElement.innerHTML = res.visits;
     });
 }
-
-updateVisitCount();

2ed9d8893bbb27dd99c123e8096b0a461e81ad53
Louie Cervantes <cervantes.louie13@gmail.com>
2023-06-27 22:22:20 -0700

    updated counter

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const countUrl = 'https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.
 async function UpdateVisitCount() {
     let response = await fetch("https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+    counter.innerHTML = data;
 }
 
 UpdateVisitCount();

ddd6b01a508303c35fa5c7bb44dbc789ca701054
Louie Cervantes <cervantes.louie13@gmail.com>
2023-06-27 22:07:39 -0700

    redid function

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const countUrl = 'https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.on.aws/'
-const countElement = document.getElementById('count');
 
-updateVisitCount();
-
-function updateVisitCount() {
-    fetch(countUrl)
-        .then(res => res.json())
-        .then(res => {
-        countElement.innerHTML = res.views;
-    });
+async function UpdateVisitCount() {
+    let response = await fetch("https://kzbrv7w7a4tbwqo4lpwywo6spa0ymrki.lambda-url.us-west-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
 }
+
+UpdateVisitCount();
